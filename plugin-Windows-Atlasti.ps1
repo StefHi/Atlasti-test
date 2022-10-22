3b14ed1a0a478b662ddf5d7223a14408e3da9a91
StefHi <stef.s.degroot@student.hu.nl>
2022-10-22 14:34:53 +0200

    initial commit

---
+++ plugin-Windows-Atlasti.ps1
@@ -0,0 +1,39 @@
+$ProgressPreference = 'SilentlyContinue'
+
+# $ErrorActionPreference = "Stop"
+ 
+function DownloadAtlasti([string] $url, [string] $downloadLocation, [int] $retries)
+{
+    while($true)
+    {
+        try
+        {
+            Invoke-WebRequest $url -OutFile $downloadLocation
+            break
+        }
+        catch
+        {
+            $exceptionMessage = $_.Exception.Message
+            Write-Host "Failed to download '$url': $exceptionMessage"
+            if ($retries -gt 0) {
+                $retries--
+                Write-Host "Waiting 10 seconds before retrying. Retries left: $retries"
+                Start-Sleep -Seconds 10
+ 
+            }
+            else
+            {
+                $exception = $_.Exception
+                throw $exception
+            }
+        }
+    }
+}
+
+try {
+    DownloadAtlasti -url https://go.microsoft.com/fwlink/p/?LinkId=2124703 -downloadLocation ".\MicrosoftEdgeWebview2Setup.exe" -retries 3
+    Start-process MicrosoftEdgeWebview2Setup.exe -Wait -ArgumentList '/silent /install'
+} catch {
+    Write-Host "Atlasti installation has failed with the following error: $_"
+    Throw "Aborted Atlasti installation returned $_"
+}
